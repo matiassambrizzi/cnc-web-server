4a381e6684338727b1314e6e171b8067d8c62bd9
matiassambrizzi <msambrizzi@fi.uba.ar>
2020-07-16 00:06:18 -0300

    Started blank arduino project

---
+++ src/config.h
@@ -0,0 +1,15 @@
+#ifndef _CONFIG__H_
+#define _CONFIG__H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/web_server.ino
@@ -0,0 +1,15 @@
+/*
+* Connecnting the ESP8622 to local newtwork
+* =========================================
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+void setup() {
+
+}
+
+void loop() {
+}
+
